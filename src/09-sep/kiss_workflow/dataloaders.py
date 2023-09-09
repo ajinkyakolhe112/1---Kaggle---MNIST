b06888c01d491ff64f30222dccd50f1b7ab9d9ad
Ajinkya Kolhe <ajinkyakolhe112@gmail.com>
2023-09-09 22:05:50 +0530

    simple end to end workflow

--- src/09-sep/kiss_workflow/dataloaders.py
+++ src/09-sep/kiss_workflow/dataloaders.py
@@ -15,19 +15,17 @@ X = train_df
 
 # Convert X in Pandas -> Numpy -> Torch Tensor
 X = torch.tensor(X.to_numpy(), dtype=torch.float32, requires_grad=False)
-y = torch.tensor(y.to_numpy())
+y = torch.tensor(y.to_numpy(), dtype=torch.int64) # torch.one_hot expects int64, int32 doesn't work
 print(f'{X.shape}{y.shape}')
 
 X = X.reshape(42000,28,28,1) 
-y_vec = torch.nn.functional.one_hot(y, 10) # one hot expects, datatype of y to be int 
-y_vec = y_vec.to(torch.float32)
 
 test_data_X = test_df.to_numpy()
 test_data_X = torch.tensor(test_data_X, dtype=torch.float32, requires_grad=False)
 test_data_X = test_data_X.reshape(-1,28,28,1)
 
 # Tensor Array to Tensor Dataset format.
-train_dataset = torch.utils.data.TensorDataset(X,y_vec) # dataloader always needs datatype = Dataset. Hence we are reformatting
+train_dataset = torch.utils.data.TensorDataset(X,y) # dataloader always needs datatype = Dataset. Hence we are reformatting
 test_dataset  = torch.utils.data.TensorDataset(test_data_X)
 
 print(f'X_train[0] = {train_dataset[0][0].shape}, Y_train[0] = {train_dataset[0][1].shape}')
@@ -38,8 +36,8 @@ print(f'X_test[0] = {test_dataset[0][0].shape}')
 train_dataset, val_dataset = torch.utils.data.random_split(train_dataset, [0.7, 0.3])
 
 options_dict = {
-	"batch_size": 7,
-	"shuffle": True,
+    "batch_size": 5,
+    "shuffle": True,
 }
 train_data_loader = torch.utils.data.DataLoader(train_dataset, **options_dict)
 val_data_loader   = torch.utils.data.DataLoader(val_dataset, **options_dict)
--- src/09-sep/kiss_workflow/simple_end-to-end.py
+++ src/09-sep/kiss_workflow/simple_end-to-end.py
@@ -4,58 +4,59 @@ from torch.nn.functional import relu
 from torch.nn.functional import softmax
 from torch.nn.functional import log_softmax
 
+import logging
+logging.basicConfig( level = logging.DEBUG, format = '[%(filename)s:%(lineno)d] %(message)s' )
+logger = logging.getLogger(__name__)
+
 class Baseline_NN(torch.nn.Module):
-	def __init__(self):
-		super().__init__()
-		
-		self.layer_1        = nn.Linear( 28*28 , 4  )  # hidden layer 1
-		self.layer_2        = nn.Linear(  4    , 15 )  # hidden layer 2
-		self.decision_maker = nn.Linear(  15   , 10 )  # output layer
-				
-	def forward(self, x_actual):
-		
-		x  = x_actual
-		x  = x.reshape(-1, 28*28)
-		
-		layer_1_output = self.layer_1(x)
-		layer_1_activation = relu(layer_1_output)
-		
-		layer_2_output = self.layer_2(layer_1_activation)
-		layer_2_activation = relu(layer_2_output)
-		
-		layer_3_output  		= self.decision_maker   (layer_2_activation)
-		softmax_probs   		= softmax          (layer_3_output, dim=1)
-		log_softmax_probs 	= log_softmax      (layer_3_output, dim=1)
-		
-		return softmax_probs, log_softmax_probs
+    def __init__(self):
+        super().__init__()
+
+        self.layer_1        = nn.Linear( 28*28 , 4  )  # hidden layer 1
+        self.layer_2        = nn.Linear(  4    , 15 )  # hidden layer 2
+        self.decision_maker = nn.Linear(  15   , 10 )  # output layer
+
+    def forward(self, x_actual):
+        
+        x  = x_actual
+        x  = x.reshape(-1, 28*28)
+        
+        layer_1_output = self.layer_1(x)
+        layer_1_activation = relu(layer_1_output)
+        
+        layer_2_output = self.layer_2(layer_1_activation)
+        layer_2_activation = relu(layer_2_output)
+        
+        layer_3_output = self.decision_maker   (layer_2_activation)
+        
+        logits = layer_3_output
+        softmax_probs       = softmax    (layer_3_output, dim=1)
+        log_softmax_probs 	= log_softmax(layer_3_output, dim=1)
+        
+        return logits, softmax_probs
 
 model 		= Baseline_NN()
 optimizer 	= torch.optim.Adam( model.parameters(), lr = 0.01 )
-loss_func	= torch.nn.functional.cross_entropy
-loss_func_log = torch.nn.functional.nll_loss
 
 from torchmetrics.classification import Accuracy
 
-for batch_no, (x_actual, y_actual) in enumerate(train_data_loader):
-	
-	y_pred_softmax, y_pred_log_softmax = model(x_actual)
-	
-	torch.set_printoptions(precision=2, sci_mode=False, linewidth=120)
-	
-	y_actual_labels = torch.argmax(y_actual, dim=1)
-	
-	loss = loss_func_log(y_pred_log_softmax,y_actual_labels)
-	loss.backward()
-	optimizer.step()
-	optimizer.zero_grad()
-	
-	accuracy = Accuracy(task ="multiclass", num_classes=10)
-	y_actual_labels = torch.argmax(y_actual, dim=1)
-	print(accuracy(y_pred_softmax, y_actual_labels))
-	print(y_pred_softmax)
-	print(f'predicted = {torch.argmax(y_pred_softmax,dim=1)}')
-	print(f'actual    = {y_actual_labels}')
-	print(accuracy(y_pred_softmax, y_actual_labels))
-	
+torch.set_printoptions(precision=2, sci_mode=False, linewidth=120)
+
+for batch_no, (x_actual, y_scalar_labels) in enumerate(train_data_loader):
+    y_pred_logits, y_pred_softmax = model(x_actual)
+    loss = torch.nn.functional.cross_entropy(y_pred_softmax, y_scalar_labels)
+    loss.backward()
+    optimizer.step()
+    optimizer.zero_grad()
+
+    accuracy        = Accuracy(task ="multiclass", num_classes=10)
+    batch_accuracy  = accuracy(y_pred_softmax, y_scalar_labels)
+    
+    y_pred_labels = torch.argmax(y_pred_softmax,dim=1)
+    logger.debug(f'predicted probs  = {y_pred_softmax}')
+    logger.debug(f'predicted labels = {y_pred_labels}')
+    logger.debug(f'actual    labels = {y_scalar_labels}')
+    logger.debug(f'batch accuracy   = {batch_accuracy}')
+
 	
 	
